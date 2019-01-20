6fd8c7ba8a9f7c2af5e896f997e96c555b91c83f
景余彪 <jingyb@chinawyny.com>
2019-01-20 18:22:42 +0800

    heihei

--- sync.sh
+++ sync.sh
@@ -1,17 +1,19 @@
-images=(`docker images |grep -v TAG | awk -F "/" '{for (i=2 ;i<=NF;i++) printf $i "/"; printf "\n" }' | awk '{print $1":"$2}'`)
 GCR_URL=k8s.gcr.io
 old_GCR_URL=gcr.io
 HUB=imdingtalk
-# 打tag传到dockerhub
+# 获取核心镜像
+images=(`docker images |grep -v TAG | awk -F "/" '{for (i=2 ;i<=NF;i++) printf $i "/"; printf "\n" }' | awk '{print $1":"$2}'`)
+# 获取额外镜像
+ext_images=(`docker images |grep -v TAG | grep -v "k8s.gcr.io" | grep -v "$HUB" | awk -F "/" '{for (i=2 ;i<=NF;i++) printf $i "/"; printf "\n" }' | awk '{print $1":"$2}'`)
+# 核心镜像到tag打
 for imageName in ${images[@]} ; do
   docker tag  $GCR_URL/$imageName $HUB/$imageName
-  if [ $? != 0 ]; then
-    docker tag  $old_GCR_URL/$imageName $HUB/$imageName
-  fi
   docker rmi $GCR_URL/$imageName
-  if [ $? != 0 ]; then
-    docker rmi $old_GCR_URL/$imageName
-  fi
-  docker push $HUB/$imageName
 done
-
+# 额外镜像打tag
+for imageName in ${ext_images[@]} ; do
+  docker tag  $old_GCR_URL/$imageName $HUB/$imageName
+  docker rmi $pld_GCR_URL/$imageName
+done
+# 打tag的镜像push到dockerhub
+docker push $HUB/$imageName
