07a021f724a287bfd0ad990994724ae8d8debf72
imdingtalk <dinhnhat9458@gmail.com>
2019-01-23 10:34:35 +0800

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# 获取最新的kubeadm版本，以便下载最新的镜像
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubeadm
 chmod +x ./kubeadm
 mv ./kubeadm /usr/local/bin/kubeadm
+# 通过以下命令获取最新镜像
 kubeadm  config images pull
 # 额外镜像
 docker pull gcr.io/kubernetes-helm/tiller:v2.12.2
